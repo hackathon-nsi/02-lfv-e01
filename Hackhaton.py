5bb6044b3c854dd0a1f4cb3834943f93a0473913
nsi-lfv-plg <80093111+nsi-lfv-plg@users.noreply.github.com>
2021-03-26 08:19:50 +0100

    Add files via upload
    First Test

---
+++ Hackhaton.py
@@ -0,0 +1,14 @@
+# initializing string
+str_in = "hello world hello raphael world Gabriel hello world hello"
+
+# printing original string 
+print("The original string is : " + str(str_in)) 
+
+# lookup Dictionary 
+repl_dict = {"hello" : "\U0001F44B", "world": "\U0001F30E" , "raphael":"\U0001F6B6" , "Gabriel":"\U0001F9CD"}
+  
+# one-liner to solve problem 
+fin_str = " ".join(repl_dict.get(e, e) for e in str_in.split())
+
+# printing result  
+print("Final String : "+ str(fin_str))
