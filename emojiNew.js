eef3d8a1be9473fef125493c9d489470213d776e
nsi-lfv-ugr <79856404+nsi-lfv-ugr@users.noreply.github.com>
2021-04-12 16:09:05 +0200

    Add files via upload

---
+++ emojiNew.js
@@ -0,0 +1,19 @@
+let h1 = document.querySelector("h1");
+let button = document.querySelector("button");
+
+button.addEventListener("click",function(){
+    strNew=h1.textContent.split(" ");
+    emojis={
+        hello:"🍔",
+        world:"🌍"                                                       
+    }
+    for(let i=0; i < strNew.length;i++){
+        for(let key in emojis){
+            if(key==strNew[i]){
+                strNew[i]=emojis[key]
+            }
+        }
+        
+    }
+    h1.textContent=strNew.join(" ");
+})
