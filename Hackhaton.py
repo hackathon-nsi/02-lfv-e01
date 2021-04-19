c0e5f78af2051567729cb141ba955c9415ec55ae
nsi-lfv-plg <80093111+nsi-lfv-plg@users.noreply.github.com>
2021-04-19 07:25:12 +0200

    Add files via upload

--- Hackhaton.py
+++ Hackhaton.py
@@ -1,13 +1,17 @@
+#-*- coding: utf-8 -*-
+
 #read file
-import requests
-reponse = requests.get('https://raw.githubusercontent.com/hackathon-nsi/h7n-nsi-02/main/textes/La%20Princesse%20de%20Cl%C3%A8ves/lpdc-partie1.txt')
-texte = reponse.text
+with open("princesse.txt") as f:
+    texte = f.read()
+    print(texte)
 
 # lookup Dictionary 
 repl_dict = {"princesse" : "\U0001F478", "PRINCESSE" : "\U0001F478", "prince": "\U0001F934" , "France":"\U0001F950"}
-  
+
 # one-liner to replace words 
 fin_texte = " ".join(repl_dict.get(e, e) for e in texte.split())
+
+f.close()
 
 # printing end result  
 print(fin_texte)
