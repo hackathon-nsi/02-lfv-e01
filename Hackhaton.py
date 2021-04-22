3cd22133aabac59f31023fac9769446d03cc7476
nsi-lfv-plg <80093111+nsi-lfv-plg@users.noreply.github.com>
2021-04-22 19:38:33 +0200

    Add files via upload

--- Hackhaton.py
+++ Hackhaton.py
@@ -1,11 +1,9 @@
-#-*- coding: utf-8 -*-
-
 # read file
-with open("princesse.txt") as fin:
+with open("princesse.txt",'r', encoding='utf-8') as fin:
     texte = fin.read()
 
 # create output file
-fout = open("output.txt","w+")
+fout = open("output.txt","w+", encoding="utf-8")
 
 # lookup Dictionary 
 repl_dict = {"princesse" : "\U0001F478", "PRINCESSE" : "\U0001F478", "prince": "\U0001F934" , "France":"\U0001F950"}
